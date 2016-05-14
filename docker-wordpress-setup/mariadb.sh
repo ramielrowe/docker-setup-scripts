21462f4e61df63ad782a188b80809e40f38d9f2a
Andrew Melton <ramielrowe@gmail.com>
2016-05-14 17:05:51 -0400

    [wordpress] pull container cleanup into each containers scripts

--- docker-wordpress-setup/mariadb.sh
+++ docker-wordpress-setup/mariadb.sh
@@ -2,6 +2,9 @@
 
 . env.sh
 
+docker stop -t 10 ${NAME}wordpress-mariadb
+docker rm -fv ${NAME}wordpress-mariadb
+
 docker run --name ${NAME}wordpress-mariadb -d \
      --volumes-from ${NAME}wordpress-mariadb-volume \
      -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
